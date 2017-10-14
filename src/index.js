2615e5a7cdf2e40b70d595224c8cab8a6fe18953
Yumeng <wym7230@gmail.com>
2017-10-14 01:32:22 -0400

    init files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './containers/App';
+import reducers from './reducers';
+import registerServiceWorker from './utils/registerServiceWorker';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
