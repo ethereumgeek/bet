5c318aeef46c73d6bf377325ce5760eadc4e8d57
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2017-10-14 01:58:04 -0400

    Add initial actions and reducers

---
+++ src/actions/actions.js
@@ -0,0 +1,36 @@
+export function setAddress(address) {
+  return {
+    type: 'SET_ADDRESS',
+    address,
+  }
+}
+
+export function setError(error) {
+  return {
+    type: 'SET_ERROR',
+    error,
+  }
+}
+
+export function setBets(bets) {
+  return {
+    type: 'SET_BETS',
+    bets,
+  }
+}
+
+export function init() {
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+  }
+
+  let address = web3.eth.defaultAddress;
+  if (address === undefined) {
+    return setError('Please unlock MetaMask and reload the page.')
+  } else {
+    return setAddress(address)
+  }
+}
